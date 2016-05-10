31378c0edfba95c21cd7a9d2f2d5e4ee2527f49f
coltaffe <clt66@zips.uakron.edu>
2016-05-10 15:17:40 -0400

    Test case
    Implemented a simple test case for the rental object

--- src/Rental_t.cpp
+++ src/Rental_t.cpp
@@ -10,7 +10,14 @@
 
 int main() {
 
-    // TODO:  Add test cases
+    {
+      Video lotr("Lord of the Rings", Video::REGULAR);
+      
+      Rental rental(lotr, 10);
+      
+      assert(rental.getDaysRented() == 10);
+      assert(rental.getVideo() == lotr);
+    }
 
     return 0;
 }
